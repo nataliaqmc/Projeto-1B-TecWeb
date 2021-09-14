aa546d2f5c4fc700b43fbe7e188e39bcc6057b3d
nataliaqmc <nataliaqmc@al.insper.edu.br>
2021-09-14 18:13:24 -0300

    função d deletar funcionando

--- notes/views.py
+++ notes/views.py
@@ -6,14 +6,7 @@ def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
-        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
         Note.objects.create(title=title, content=content)
-        
-        '''note = Note()
-        note.title = title
-        note.content = content
-        note.save()'''
-
         return redirect('index')
     else:
         all_notes = Note.objects.all()
@@ -33,5 +26,5 @@ def delete(request, id):
     if request.method == 'POST':
         note = Note.objects.get(id=id)
         note.delete()
-    return redirect('index')
+        return redirect('index')
     

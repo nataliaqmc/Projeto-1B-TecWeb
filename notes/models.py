cecb8d2bf56fc48a48e8b35a9ac615832f042547
nataliaqmc <nataliaqmc@al.insper.edu.br>
2021-09-17 12:08:12 -0300

    adicionando tags

--- notes/models.py
+++ notes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tag = models.CharField(max_length=200)
     def __str__(self):
         #ID.TITULO - RETURN
         return f'{self.id}.{self.title}'
--- notes/views.py
+++ notes/views.py
@@ -6,8 +6,9 @@ def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
+        tag = request.POST.get('tag')
         # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
-        Note.objects.create(title=title, content=content)
+        Note.objects.create(title=title, content=content, tag=tag)
         return redirect('index')
     else:
         all_notes = Note.objects.all()
@@ -17,15 +18,15 @@ def update(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
+        tag = request.POST.get('tag')
         id = request.POST.get('identificador')
         note = Note.objects.get(id=id)
-        if title == '':
-            note.content = content
-        elif content == '':
+        if title != '':
             note.title = title
-        elif title != '' and content != '':
-            note.title = title
+        if content != '':
             note.content = content
+        if tag != '':
+            note.tag = tag
         note.save()
         return redirect('index')
 
